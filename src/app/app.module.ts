04eb81bee19902148d0fac4eb203b60259e620bc
Viktoriia Honcharova <vika.honcharova.619853@gmail.com>
2023-03-13 10:43:58 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
+
+@NgModule({
+  declarations: [AppComponent, UserlistComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
